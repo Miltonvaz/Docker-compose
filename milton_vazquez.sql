f0d57a760decf3e7bc7e1d6062db6aa614b3f98f
Miltonvaz <233427@ids.upchiapas.edu.mx>
2025-10-24 20:33:20 -0600

    update

--- milton_vazquez.sql
+++ milton_vazquez.sql
@@ -1,5 +1,25 @@
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgrest') THEN
+      CREATE USER postgrest WITH ENCRYPTED PASSWORD 'postgres';
+   END IF;
+END
+$do$;
 
-CREATE DATABASE "Milton_Vazquez" OWNER postgrest;
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'Milton_Vazquez') THEN
+      CREATE DATABASE "Milton_Vazquez" OWNER postgrest;
+   END IF;
+END
+$do$;
+
+GRANT ALL PRIVILEGES ON DATABASE "Milton_Vazquez" TO postgrest;
+
+\c "Milton_Vazquez"
+
 CREATE TABLE IF NOT EXISTS estudiantes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -7,3 +27,6 @@ CREATE TABLE IF NOT EXISTS estudiantes (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+INSERT INTO estudiantes (name, description)
+VALUES ('Milton Vazquez', 'Estudiante');
